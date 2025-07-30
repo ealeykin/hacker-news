6261ec2adb20ebcc3a4dc6f4aa3474a5ff686719
Eugene Aleykin <ialeikin@csod.com>
2025-07-30 19:47:22 +0200

    Skeleton

---
+++ src/HackerNews.Host/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+await app.RunAsync();
---
+++ tests/HackerNews.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace HackerNews.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
