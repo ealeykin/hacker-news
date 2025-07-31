a65ac1c9a6894c0cf6cf5872fff62f110dfd37cc
Eugene Aleykin <ialeikin@csod.com>
2025-07-31 10:49:28 +0200

    review

--- src/HackerNews.Application/Handlers/GetTopStories/GetTopStoriesHandler.cs
+++ src/HackerNews.Application/Handlers/GetTopStories/GetTopStoriesHandler.cs
@@ -1,12 +1,15 @@
 using System.Collections.Concurrent;
 using HackerNews.HackerNews.Application.Abstractions;
 using HackerNews.HackerNews.Application.Contract;
+using Microsoft.Extensions.Logging;
 
 namespace HackerNews.HackerNews.Application.Handlers.GetTopStories;
 
-public class GetTopStoriesHandler(IStoriesService storiesService) : IHandler<GetTopStoriesRequest, IReadOnlyCollection<Story>>
+public class GetTopStoriesHandler(
+    ILogger<GetTopStoriesHandler> logger,
+    IStoriesService storiesService) : IHandler<GetTopStoriesRequest, IReadOnlyCollection<Story>>
 {
-    private const int MacDegreeOfParallelism = 4;
+    private const int MaxDegreeOfParallelism = 4;
     
     public async Task<IReadOnlyCollection<Story>> InvokeAsync(GetTopStoriesRequest request, CancellationToken cancellationToken)
     {
@@ -16,13 +19,20 @@ public class GetTopStoriesHandler(IStoriesService storiesService) : IHandler<Get
         var result = new ConcurrentBag<Story>();
         var options = new ParallelOptions
         {
-            MaxDegreeOfParallelism = MacDegreeOfParallelism,
+            MaxDegreeOfParallelism = MaxDegreeOfParallelism,
             CancellationToken = cancellationToken
         };
 
         await Parallel.ForEachAsync(storyIds, options, async (storyId, ct) =>
         {
-            result.Add(await storiesService.GetStoryAsync(storyId, ct));
+            try
+            {
+                result.Add(await storiesService.GetStoryAsync(storyId, ct));
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to get story. StoryId: {StoryId}", storyId);
+            }
         });
         
         return result
